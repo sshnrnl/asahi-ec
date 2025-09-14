82cf3b67781544b389b13a99068bfe8de6aa3d79
Sebastian Shan Reinaldo <sebastianshanreinaldo@gmail.com>
2025-09-14 23:07:09 +0700

    [Feat] Added whatsapp button

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,7 @@ import { Session, getServerSession } from "next-auth";
 import { authOptions } from "@/libs/auth";
 import { getTotalItems } from "./(carts)/cart/action";
 import { getTotalWishlist } from "./(carts)/wishlist/action";
+import { Phone } from "lucide-react";
 
 import "../styles/globals.css";
 
@@ -18,7 +19,7 @@ export const metadata: Metadata = {
   description: "JUAL IPAL BIO ASAHI FIBREGLASS TERPERCAYA DI INDONESIA",
   icons: {
     icon: "/favicon.ico",
-    shortcut: "/favicon.ico", 
+    shortcut: "/favicon.ico",
   },
 };
 
@@ -47,6 +48,22 @@ export default async function RootLayout({
             <SpeedInsights />
           </main>
           {/* <Footer /> */}
+
+          <div className="z-10 flex items-center gap-2 h-min py-3 px-5 rounded-md bg-green-500 w-min fixed bottom-4 right-4">
+            <a
+              className="flex items-center gap-2"
+              href={`https://api.whatsapp.com/send?phone=6282116600600&text=${encodeURIComponent(
+                "Terima kasih telah menghubungi PT. Asahi Fibreglass. Kami produsen IPAL, STP, Tangki Air, Tangki Kimia, Septic Tank & produk fiberglass lainnya.\n\nMohon isi data berikut,\n\n\t1.	Nama :\n\t2.	Perusahaan (PT) :\n\t3.	Lokasi Proyek :\n\t4.	Kebutuhan : \n\nTim kami akan segera menghubungi Anda 🙏🏻"
+              )}`}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <div className="z-50 border-2 p-2 rounded-full ">
+                <Phone color="#f5f5f5" size={20} strokeWidth={2} />
+              </div>
+              <p className=" font-bold text-neutral-100">Marketing</p>
+            </a>
+          </div>
         </body>
       </Providers>
     </html>
